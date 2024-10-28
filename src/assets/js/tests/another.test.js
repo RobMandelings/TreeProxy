64c6688aac37eaa28561e0f5f20d27faf0dc711f
Rob Mandelings <rob@minetronic.com>
2024-10-28 11:27:13 +0100

    Testing works

--- src/assets/js/tests/another.test.js
+++ src/assets/js/tests/another.test.js
@@ -1,3 +1,16 @@
+import {reactive} from "vue";
+import {SourceNodeMap} from "../NodeMap.js";
+import {CustomNode} from "../CustomNode.js";
+import * as Proxies from "../Proxies.js";
+
+
 test('hello', () => {
-    expect(1 + 2).toBe(3)
+    const sourceNodeMap = reactive(new SourceNodeMap());
+    const child2Id = sourceNodeMap.addNode(new CustomNode('Child 2'));
+    const rootId = sourceNodeMap.addNode(new CustomNode('Root', [child2Id]));
+    const srcTree = Proxies.createSourceTree(sourceNodeMap, rootId);
+
+
+    expect(srcTree.children[0].parent.children[0].id).toBe(child2Id)
+
 })
