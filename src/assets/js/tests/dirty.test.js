a183bde351ec7dca58259ba0ea0068f364514446
Rob Mandelings <rob@minetronic.com>
2024-11-06 10:29:29 +0100

    Added some tests for dirty props

--- src/assets/js/tests/dirty.test.js
+++ src/assets/js/tests/dirty.test.js
@@ -1,5 +1,4 @@
 import {SourceTree} from "../proxy_tree/SrcTree.js";
-import {createTree} from "./TreeUtil.js";
 import {OverlayTree} from "../proxy_tree/OverlayTree.js";
 
 describe('', () => {
@@ -8,10 +7,23 @@ describe('', () => {
     beforeEach(() => {
         srcTree = new SourceTree().init(0);
         ovTree = new OverlayTree(srcTree);
-        // expect(srcTree.root.isDirty)
+        expect(srcTree.root.dirty).toBe(false);
+        expect(ovTree.root.dirty).toBe(false);
     })
 
-    test('', () => {
-        // srcTree.score.dirty
+    test('Name change src', () => {
+        srcTree.root.name.value = "Changed";
+        expect(srcTree.root.name.dirty).toBe(false);
+        expect(ovTree.root.name.value).toBe(srcTree.root.name.value);
+        expect(ovTree.root.name.dirty).toBe(false);
+    });
+
+    test('Name change ov', () => {
+        ovTree.root.name.value = "Changed";
+        expect(ovTree.root.dirty).toBe(true);
+        expect(ovTree.root.name.dirty).toBe(true);
+        expect(srcTree.root.name.value).not.toBe("Changed");
+        expect(srcTree.root.name.dirty).toBe(false);
+        expect(srcTree.root.dirty).toBe(false);
     });
 })
