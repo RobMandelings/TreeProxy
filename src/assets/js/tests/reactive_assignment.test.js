005212a145d1fd73e6576baefd9956c8abe94cce
Rob Mandelings <rob@minetronic.com>
2024-11-16 19:17:24 +0100

    Reactive assignment improved

---
+++ src/assets/js/tests/reactive_assignment.test.js
@@ -0,0 +1,59 @@
+import {isProxy, reactive, toRaw} from "vue";
+import {createSourceTree} from "@/SimpleProxyTreeBuilders.js";
+
+describe('Tests', () => {
+
+    let react;
+    beforeEach(() => {
+        react = reactive({value: null});
+    })
+
+    test('Simple proxy test', () => {
+
+        const p = new Proxy({}, {});
+        react.value = p;
+        expect(toRaw(react.value)).toBe(p);
+
+    })
+
+    test('ToRaw', () => {
+
+        const p = new Proxy({}, {});
+        const raw = toRaw(p);
+        expect(isProxy(raw)).toBe(false);
+    })
+
+    test('ToRaw2', () => {
+
+        const p = new Proxy(reactive({}), {});
+        const raw = toRaw(p);
+        expect(isProxy(raw)).toBe(false);
+    })
+
+    test('Reactive assignment', () => {
+
+        const react = reactive({value: null});
+
+        const p = new Proxy(reactive({}), {});
+        const raw = toRaw(p);
+        react.value = p;
+        expect(isProxy(raw)).toBe(false);
+    })
+
+    test('Reactive assignment workaround', () => {
+
+        const react = reactive({value: null});
+
+        const t = {};
+        const p = new Proxy(reactive({}), {
+            get(target, p, receiver) {
+                if (p === "__v_raw") return undefined;
+                const normalResult = Reflect.get(target, p, receiver);
+                return Reflect.get(target, p, receiver);
+            }
+        });
+        const raw = toRaw(p);
+        react.value = p;
+        expect(isProxy(raw)).toBe(false);
+    })
+})
